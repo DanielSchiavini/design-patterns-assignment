98ec4fa74fe6e136aec76bf20ee70806430f1931
Daniel Schiavini <daniel.schiavini@gmail.com>
2018-10-27 19:22:11 +0200

    Moving docker pull to shell script

--- docker-latex.sh
+++ docker-latex.sh
@@ -3,8 +3,10 @@ mkdir output 2>/dev/null
 
 export LATEX_IMAGE="blang/latex:ubuntu"
 
+docker pull ${LATEX_IMAGE}
+
 docker run --rm --interactive --tty \
     --network=none \
     --volume "${PWD}":/data \
-    "${LATEX_IMAGE}" \
+    ${LATEX_IMAGE} \
     ./build-latex.sh
