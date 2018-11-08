4d70ca3f3b89844634926c98de8dab300090435d
Daniel Schiavini <daniel.schiavini@gmail.com>
2018-11-08 14:01:25 +0100

    Fixing references in generated report

--- build-latex.sh
+++ build-latex.sh
@@ -2,6 +2,10 @@
 mkdir output 2>/dev/null
 
 cd Report
+# run first to find references
+pdflatex -output-directory=../output final-assignment-report.tex
+
+# run again to fix references
 pdflatex -output-directory=../output final-assignment-report.tex
 cd ..
 
