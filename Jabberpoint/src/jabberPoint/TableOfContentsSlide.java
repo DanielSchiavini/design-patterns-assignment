ea732d65dcafbce91a064caf325c7917ff438c4d
Daniel Schiavini <daniel.schiavini@gmail.com>
2018-11-13 22:20:24 +0100

    Adding new PR links

--- Jabberpoint/src/jabberPoint/TableOfContentsSlide.java
+++ Jabberpoint/src/jabberPoint/TableOfContentsSlide.java
@@ -79,7 +79,7 @@ public class TableOfContentsSlide extends Slide {
 				subject = slide.getTitle();
 			}
 
-			if (subject != lastSubject) {
+			if (!subject.equals(lastSubject)) {
 				// the subject changed, let's add it to the slide items.
 				int level = isCurrent ? 1 : 2;
 				append(level, subject);
